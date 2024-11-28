56162bcbd9d16a8ce7df8c34c7f663b04599824e
autest <gokulakrishnan@mmhglobal.com>
2024-11-28 22:50:06 +1300

    Sprint 16 Script Updates

--- src/test/java/Happy_Path_Provider_Web/pages/SMSSettingsPage.java
+++ src/test/java/Happy_Path_Provider_Web/pages/SMSSettingsPage.java
@@ -1,8 +1,9 @@
 package Happy_Path_Provider_Web.pages;
 
+import cap.common.BasePage;
 import org.openqa.selenium.WebDriver;
 
-public class SMSSettingsPage {
+public class SMSSettingsPage extends BasePage {
 
     public SMSSettingsPage(WebDriver driver) {
         super(driver);
